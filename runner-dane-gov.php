5aae876f1d827a860e147195ed5afd6aa4175c97
Creastv <creastv@gail.com>
2025-08-28 17:31:22 +0200

    start project

---
+++ runner-dane-gov.php
@@ -0,0 +1,5 @@
+<?php
+// /home/jffrbblxkx/domains/redmar.jffrbblxkx.cfolks.pl/public_html/wp-content/plugins/jawnosc-cen/runner-dane-gov.php
+require dirname(__DIR__, 2) . '/wp-load.php'; // dostosuj ścieżkę jeśli plik jest gdzie indziej
+do_action('dane_gov_exporter_daily');
+echo "ok\n";
